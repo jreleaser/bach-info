d565e3f5ebe59776012ff339c7433ea6dad04bbf
Christian Stein <sormuras@gmail.com>
2024-05-22 15:37:33 +0200

    Move import package element name to last position

--- JReleaser.java
+++ JReleaser.java
@@ -1,10 +1,10 @@
-package org.jreleaser.bach.info;
+package bach.info.org.jreleaser;
 
 /** JReleaser 1.12.0 */
 public interface JReleaser {
   String RELEASES = "https://github.com/jreleaser/jreleaser/releases/download";
   String VERSION = "1.12.0";
-  String MODULES = // language=properties
+  String PROPERTIES = // language=properties
       """
       org.jreleaser=${RELEASES}/v${VERSION}/jreleaser-tool-provider-${VERSION}.jar\
       #SIZE=49421454&SHA-256=1c5b7f26cb27253dd205d877e55d57e013c88652aa15f7e7185e11fa2012fc2c
--- package-info.java
+++ package-info.java
@@ -1,2 +1,2 @@
 /** Information about modules and tools provided by JReleaser 1.12.x. */
-package org.jreleaser.bach.info;
+package bach.info.org.jreleaser;
