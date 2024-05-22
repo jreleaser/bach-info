796cf4747947a6fe864bfe2e6c479ca65c72a1c7
Christian Stein <sormuras@gmail.com>
2024-05-22 16:52:34 +0200

    Re-flow link constant to a valid URL

--- JReleaser.java
+++ JReleaser.java
@@ -6,11 +6,11 @@ package bach.info.org.jreleaser;
  * @see <a href="https://github.com/jreleaser/jreleaser/releases/tag/v1.12.0">Release Notes</a>
  */
 public interface JReleaser {
-  String RELEASES = "https://github.com/jreleaser/jreleaser/releases/download";
+  String RELEASES = "https://github.com/jreleaser/jreleaser/releases";
   String VERSION = "1.12.0";
   String PROPERTIES = // language=properties
       """
-      org.jreleaser=${RELEASES}/v${VERSION}/jreleaser-tool-provider-${VERSION}.jar\
+      org.jreleaser=${RELEASES}/download/v${VERSION}/jreleaser-tool-provider-${VERSION}.jar\
       #SIZE=49421454&SHA-256=1c5b7f26cb27253dd205d877e55d57e013c88652aa15f7e7185e11fa2012fc2c
       """
           .replace("${RELEASES}", RELEASES)
