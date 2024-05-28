7bdd3dc9bf5eee7f1ce0104908242108aee12bb2
Christian Stein <sormuras@gmail.com>
2024-05-28 12:39:42 +0200

    Implement module locator SPI

--- JReleaser.java
+++ JReleaser.java
@@ -1,5 +1,7 @@
 package run.info.org.jreleaser;
 
+import run.bach.ModuleLocator;
+
 /**
  * JReleaser 1.12.0
  *
@@ -7,27 +9,8 @@ package run.info.org.jreleaser;
  */
 public interface JReleaser {
   String RELEASES = "https://github.com/jreleaser/jreleaser/releases";
-  String NAME = "jreleaser";
   String VERSION = "1.12.0";
 
-  String TOOL_PROVIDER_URI = // TODO Convert to use String Templates
-      """
-      ${RELEASES}/download/v${VERSION}/${NAME}-tool-provider-${VERSION}.jar\
-      #SIZE=49421454\
-      &SHA-256=1c5b7f26cb27253dd205d877e55d57e013c88652aa15f7e7185e11fa2012fc2c"""
-          .replace("${RELEASES}", RELEASES)
-          .replace("${NAME}", NAME)
-          .replace("${VERSION}", VERSION);
-
-  String MODULES_PROPERTIES = // language=properties
-      """
-      org.jreleaser.tool=${TOOL_PROVIDER_URI}
-      """;
-  String MODULES = MODULES_PROPERTIES.replace("${TOOL_PROVIDER_URI}", TOOL_PROVIDER_URI);
-
-  String APPLICATION = TOOL_PROVIDER_URI;
-  String APPLICATION_ASSETS = // language=properties
-      """
-      README.adoc=https://github.com/jreleaser/jreleaser/raw/v1.12.0/README.adoc#SIZE=6162
-      """;
+  String URI = RELEASES + "/download/v" + VERSION + "/jreleaser-tool-provider-" + VERSION + ".jar";
+  ModuleLocator MODULES = ModuleLocator.of("org.jreleaser.tool", URI);
 }
